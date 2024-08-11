2f83303e0edb84ffc102498214d9e5381fc74ab1
Deepanshu Taneja <deepanshutaneja762@gmail.com>
2024-08-11 18:56:40 +0530

    asd

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ dotenv.config();
 const app = express();
 const MONGO_URI = process.env.MONGO_URI;
 
+app.get('/', (req, res) => {
+  res.json('API is working!');
+});
+
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB!'))
   .catch(err => console.error('Failed to connect to MongoDB:', err));
